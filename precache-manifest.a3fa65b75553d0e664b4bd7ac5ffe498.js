4cbc1a4ced34d29b27c674f46c60dc39f5f050e6
Bhaskar Jyoti Kherkatary <kherkatarybhaskar@gmail.com>
2021-03-14 12:38:21 +0530

    Updates

---
+++ precache-manifest.a3fa65b75553d0e664b4bd7ac5ffe498.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b49698e431875fa83b01a51ca45b6d6b",
+    "url": "/odmtestapp/index.html"
+  },
+  {
+    "revision": "de05103f94625440fa62",
+    "url": "/odmtestapp/static/css/3.56d8f420.chunk.css"
+  },
+  {
+    "revision": "bc342d7fedcecde29357",
+    "url": "/odmtestapp/static/css/main.ed6d0169.chunk.css"
+  },
+  {
+    "revision": "e50227b9897b2ce41338",
+    "url": "/odmtestapp/static/js/2.e55735f0.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/odmtestapp/static/js/2.e55735f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de05103f94625440fa62",
+    "url": "/odmtestapp/static/js/3.1b853be2.chunk.js"
+  },
+  {
+    "revision": "bc342d7fedcecde29357",
+    "url": "/odmtestapp/static/js/main.1efbd4d8.chunk.js"
+  },
+  {
+    "revision": "dafb8c129ad97c6a625a",
+    "url": "/odmtestapp/static/js/runtime-main.634f8fd8.js"
+  },
+  {
+    "revision": "cc9495e9e9e51452e26e413901a7d72b",
+    "url": "/odmtestapp/static/media/attaimage.cc9495e9.png"
+  },
+  {
+    "revision": "668b8e05db3188ae782bed865dc04e93",
+    "url": "/odmtestapp/static/media/logo.668b8e05.jpg"
+  }
+]);
